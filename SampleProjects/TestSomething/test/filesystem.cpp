2d4e0c4c15ef3e9dec349c607eda6d047455a2a1
James Foster <git@jgfoster.net>
2021-02-16 16:37:45 -0800

    Show current path.

--- SampleProjects/TestSomething/test/filesystem.cpp
+++ SampleProjects/TestSomething/test/filesystem.cpp
@@ -7,6 +7,8 @@ namespace fs = std::experimental::filesystem;
 unittest(exists)
 {
   assertTrue(fs::exists("."));
+  std::string path = fs::current_path();
+  std::cout << "path = " << path << std::endl;
 }
 
 unittest_main()
